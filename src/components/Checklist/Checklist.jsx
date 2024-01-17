9e769b948cb953564331f148457846d9eb993577
Vendula Krajíčková <vendysacek@seznam.cz>
2024-01-17 13:36:42 +0100

    done add picture

--- src/components/Checklist/Checklist.jsx
+++ src/components/Checklist/Checklist.jsx
@@ -4,9 +4,9 @@ import { Item } from '../Item/Item';
 export const Checklist = () => {
   return (
     <ul>
-      <Item text="Frontend" />
-      <Item text="Styled" />
-      <Item text="Bundler" />
+      <Item text="Frontend" src="./img/vite_header.jpg" />
+      <Item text="Styled" src="" />
+      <Item text="Bundler" src="" />
       {/* <Item text="Frontend" />
       <Item text="Styled" />
       <Item text="Bundler" />
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export const Item = ({ text }) => {
+export const Item = ({ text, src }) => {
   return (
-    <li className="my-2 p-2 bg-white hover:border-[#5881fd] text-[#1d284c] leading-snug cursor-pointer border-[2px] border-[#eaebee] rounded-lg">{ text }</li>
+    <li className="my-2 bg-white hover:border-[#5881fd] text-[#1d284c] leading-snug cursor-pointer border-[2px] border-[#eaebee] rounded-lg">
+      {
+        src && <figure className="flex items-center"><img className="max-h-52 sm:max-h-36 rounded-t-lg" src={src} alt="Vite Header" /></figure>
+      }
+      <div className="p-2">
+        <p>
+          { text }
+        </p>
+      </div>
+    </li>
   )
 }
