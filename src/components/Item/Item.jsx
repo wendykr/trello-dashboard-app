01d83d14386b6f38facfea6f2e72ed7953131206
Vendula Krajíčková <vendysacek@seznam.cz>
2024-01-17 14:32:42 +0100

    done label

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Label } from '../Label/Label';
 
 export const Item = ({ text, src }) => {
   return (
@@ -7,6 +8,10 @@ export const Item = ({ text, src }) => {
         src && <figure className="flex items-center"><img className="max-h-52 sm:max-h-36 rounded-t-lg" src={src} alt="Vite Header" /></figure>
       }
       <div className="p-2">
+        <aside className="mt-0.5 mb-1.5 flex">
+          <Label color="yellow" />
+          <Label color="blue" />
+        </aside>
         <p>
           { text }
         </p>
---
+++ src/components/Label/Label.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Label = ({ color }) => {
+  return (
+    <span className={`w-10 h-2 mr-2 rounded-full bg-${color}-400`}></span>
+  )
+}
