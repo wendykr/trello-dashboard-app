edce96d63f2d4c1b50ea81f6f04498a0be17a53e
Vendula Krajíčková <vendysacek@seznam.cz>
2024-01-18 12:33:13 +0100

    hidden icon edit

--- src/components/LinkEdit/LinkEdit.jsx
+++ src/components/LinkEdit/LinkEdit.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const LinkEdit = () => {
   return (
-    <a href="#" className="w-8 h-8 p-2 bg-white hover:bg-[#f1f2f4] text-[#44546f] rounded-full flex justify-center items-center absolute top-0.5 right-0.5">
+    <a href="#" className="hidden w-8 h-8 p-2 bg-white hover:bg-[#f1f2f4] text-[#44546f] rounded-full flex justify-center items-center absolute top-0.5 right-0.5">
       <svg fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
       </svg>
