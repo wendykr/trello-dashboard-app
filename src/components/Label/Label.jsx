e860290bc076ed6b1998536b8dbfe1b3a9777e06
Vendula Krajíčková <vendysacek@seznam.cz>
2024-01-20 18:11:49 +0100

    done show label detail

--- src/components/Label/Label.jsx
+++ src/components/Label/Label.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-export const Label = ({ color }) => {
-
+export const Label = ({ color, title, onClickLabel, isClicked }) => {
   return (
-    <span style={{ backgroundColor: `#${color}` }} className="w-10 h-2 mr-2 rounded-full"></span>
-  )
-}
+    <span
+      style={{ backgroundColor: `#${color}` }}
+      className={isClicked ? 'min-w-10 h-auto mr-2 px-3 py-0.5 rounded-[4px] text-[10px] text-center' : 'w-10 h-2 mr-2 rounded-full text-[10px] text-center'}
+      onClick={onClickLabel}
+    >
+      {isClicked ? title : ''}
+    </span>
+  );
+};
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from '../../components/Card/Card';
 import { NewColumn } from '../../components/NewColumn/NewColumn';
 import { FormColumn } from '../../components/FormColumn/FormColumn';
 import { tasksData } from '../../constants/tasks';
 
 export const DashboardPage = () => {
+  const [isClickLabel, setIsClickLabel] = useState(false);
+
+  const handleClickLabel = () => {
+    setIsClickLabel((prev) => !prev);
+  };
 
   return (
     <main className="flex bg-gradient-to-br from-[#228cd5] via-[#228cd5] to-[#37B4C3]">
@@ -12,7 +17,13 @@ export const DashboardPage = () => {
         <div id="overlay" className="hidden fixed top-0 left-0 w-full h-screen bg-black bg-opacity-70 z-10"></div>
         {
           tasksData.map(oneTask => (
-            <Card title={oneTask.title} key={oneTask.id} cards={oneTask.cards} />
+            <Card
+              title={oneTask.title}
+              cards={oneTask.cards}
+              key={oneTask.id} 
+              isClickLabel={isClickLabel}
+              onClickLabel={handleClickLabel}
+            />
           ))
         }
         <div>
