f7c137e7f1d1cfbeb47d2b9f35fdeea1e385797c
Vendula Krajíčková <vendysacek@seznam.cz>
2024-01-21 05:58:11 +0100

    add event onClickAddCard

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -5,7 +5,7 @@ import { LinkClose } from '../LinkClose/LinkClose';
 
 export const Form = () => {
   return (
-    <form action="" className="hidden m-2">
+    <form action="" className="m-2">
       <Textarea rows="3" placeholder="Zadej název pro tuto kartu..." shadow="shadow-[0_1px_0px_rgba(9,30,66,0.3)]" />
       <div className="flex flex-row items-center gap-1">
         <Button text="Přidat kartu" />
