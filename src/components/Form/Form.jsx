fb866bff6df9759556ffb2b05b014d999a2fc161
Vendula Krajíčková <vendysacek@seznam.cz>
2024-01-21 09:10:00 +0100

    hidden form after click close

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -3,13 +3,13 @@ import { Button } from '../Button/Button';
 import { Textarea } from '../Textarea/Textarea';
 import { LinkClose } from '../LinkClose/LinkClose';
 
-export const Form = () => {
+export const Form = ({ onClickLinkClose }) => {
   return (
     <form action="" className="m-2">
       <Textarea rows="3" placeholder="Zadej název pro tuto kartu..." shadow="shadow-[0_1px_0px_rgba(9,30,66,0.3)]" />
       <div className="flex flex-row items-center gap-1">
         <Button text="Přidat kartu" />
-        <LinkClose />
+        <LinkClose onClickLinkClose={onClickLinkClose} />
       </div>
     </form>
   )
