a84b453ba3e90165c1bf915f69824077b087a082
Vendula Krajíčková <vendysacek@seznam.cz>
2024-02-09 09:43:41 +0100

    title for button

--- src/components/ButtonCopy/ButtonCopy.jsx
+++ src/components/ButtonCopy/ButtonCopy.jsx
@@ -5,7 +5,7 @@ export const ButtonCopy = ({ handleClickCopy }) => {
   return (
     <button
       className="p-2 hover:bg-[#091e4224] text-[#44546f] rounded-lg flex justify-center items-center"
-      title="Kopírovat"
+      title="Duplikovat"
       onClick={handleClickCopy}>
       <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
         <path
